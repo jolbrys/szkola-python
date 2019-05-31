1f457555977c797fd7a91eec22fc891ade2dc00a
jolbrys <jakub.olbrys@gmail.com>
2019-05-31 13:06:00 -0700

    first commit

---
+++ django2/djangocrud/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from djangocrud.api import views
+
+router = routers.DefaultRouter()
+router.register(r'movies', views.MovieViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
